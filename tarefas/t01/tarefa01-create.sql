3b07883795ab3d68cc7e9072eea32b903d4320e2
brennovictor <77638452+brennovictor@users.noreply.github.com>
2022-05-10 10:51:14 -0300

    criação da tabela funcionario

--- tarefas/t01/tarefa01-create.sql
+++ tarefas/t01/tarefa01-create.sql
@@ -1,1 +1,9 @@
-
+CREATE TABLE funcionario (
+  codigo INTEGER NOT NULL,
+  nome varchar(15) NOT NULL,
+  sexo char(1) DEFAULT NULL,
+  dataNasc date DEFAULT NULL,
+  salario decimal(10,2) DEFAULT NULL,
+  cod_depto INTEGER DEFAULT NULL,
+  PRIMARY KEY (codigo)
+);
