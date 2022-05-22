16389eaa4da8257ab1c0d5cf094ca7f2459e789f
brennovictor <77638452+brennovictor@users.noreply.github.com>
2022-05-22 19:43:48 -0300

    Update tarefa02-q05.sql

--- tarefas/t02/tarefa02-q05.sql
+++ tarefas/t02/tarefa02-q05.sql
@@ -1,1 +1,18 @@
+/*05. Crie um gatilho que ao se adicionar um voo, seja adicionado +1 voo para o piloto
+daquele voo. */
 
+CREATE FUNCTION add_voo_piloto()
+RETURNS trigger AS $$
+BEGIN
+    UPDATE piloto 
+    SET num_voos = num_voos + 1
+    WHERE codigo = NEW.cod_piloto;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER add_voo
+AFTER INSERT ON voo
+    FOR EACH ROW 
+    EXECUTE FUNCTION add_voo_piloto();
