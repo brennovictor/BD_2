51e9b95132f4ea38e8d08bea3b4df280c6aedd20
brennovictor <77638452+brennovictor@users.noreply.github.com>
2022-05-22 20:27:25 -0300

    Create tarefa02-q06.sql

---
+++ tarefas/t02/tarefa02-q06.sql
@@ -0,0 +1,21 @@
+/* 06. Crie um gatilho que some milhas ao cartão de milhagem sempre que um cliente é
+adicionado a um voo. (considere que é adicionado 1 milha no cartão para cada 10
+milhas de distância percorrida em um voo). */
+
+CREATE FUNCTION soma_milhas_cliente()
+RETURNS trigger AS $$
+BEGIN
+    UPDATE milhas 
+    SET quantidade = quantidade + (SELECT distancia FROM voo WHERE codigo = (SELECT voo FROM cliente_voo))
+    WHERE cliente = NEW.cliente;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER soma_milhas
+AFTER INSERT ON cliente_voo
+    FOR EACH ROW 
+    EXECUTE FUNCTION soma_milhas_cliente();
+   
+   
