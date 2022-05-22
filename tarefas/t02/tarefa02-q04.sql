b18585389fd44ffdfc610e4386a3d3443dee9b3f
brennovictor <77638452+brennovictor@users.noreply.github.com>
2022-05-22 19:04:00 -0300

    Update tarefa02-q04.sql

--- tarefas/t02/tarefa02-q04.sql
+++ tarefas/t02/tarefa02-q04.sql
@@ -1,1 +1,16 @@
+/* 04. Crie um gatilho que ao se adicionar um novo cliente, seja criado para ele um cartão de milhagem com 0 milhas. */
 
+CREATE FUNCTION inserir_cartao_milhas()
+RETURNS trigger AS $$
+BEGIN
+    INSERT INTO milhas VALUES(cliente, quantidade);
+    VALUES(NEW.codigo, 0);
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER criar_cartao_cliente
+AFTER INSERT ON cliente
+    FOR EACH ROW 
+    EXECUTE FUNCTION inserir_cartao_milhas();
