26668a3519f35b4ea5a4860583dcae34a11beb29
brennovictor <77638452+brennovictor@users.noreply.github.com>
2022-05-22 19:01:28 -0300

    creates

--- tarefas/t02/creates.sql
+++ tarefas/t02/creates.sql
@@ -1,1 +1,39 @@
+CREATE TABLE cliente ( 
+	codigo SERIAL NOT NULL,
+	nome VARCHAR(50) NOT NULL, 
+	endereco VARCHAR(100) NOT NULL,
+	PRIMARY KEY (codigo) 
+);
 
+CREATE TABLE piloto ( 
+	codigo SERIAL NOT NULL,
+	nome VARCHAR(50) NOT NULL, 
+	num_voos INTEGER NOT NULL,
+	PRIMARY KEY (codigo)
+);
+
+CREATE TABLE voo ( 
+	codigo SERIAL NOT NULL,
+        tipo VARCHAR(10) NOT NULL,
+	cod_piloto INTEGER NOT NULL,
+	distancia DECIMAL(10,2) NOT NULL,
+	PRIMARY KEY (codigo),
+	FOREIGN KEY (cod_piloto) REFERENCES piloto(codigo)
+);
+
+CREATE TABLE milhas (
+        cliente INTEGER NOT NULL,
+        quantidade INTEGER NOT NULL,
+        PRIMARY KEY(cliente),
+        FOREIGN KEY(cliente) REFERENCES cliente(codigo)
+
+);
+
+CREATE TABLE cliente_voo (
+        cliente INTEGER NOT NULL,
+        voo INTEGER NOT NULL,
+        classe VARCHAR(5) NOT NULL,
+        PRIMARY KEY(cliente, voo),
+        FOREIGN KEY(cliente) REFERENCES cliente(codigo),
+        FOREIGN KEY(voo) REFERENCES voo(codigo)
+);
