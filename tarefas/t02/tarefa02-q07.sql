eb9177f6878ba16e026f2a5d07da608a1409db09
brennovictor <77638452+brennovictor@users.noreply.github.com>
2022-05-23 17:25:14 -0300

    Update tarefa02-q07.sql

--- tarefas/t02/tarefa02-q07.sql
+++ tarefas/t02/tarefa02-q07.sql
@@ -1,1 +1,18 @@
+/* 07. Crie um gatilho que adicione mais um passageiro a um voo sempre que um cliente
+entre em um voo. (Dica: lembre da relação cliente_voo). */
 
+CREATE FUNCTION add_num_passageiros()
+RETURNS trigger AS $$
+BEGIN
+    UPDATE voo
+    SET num_passageiros = num_passageiros + 1
+    WHERE codigo = NEW.voo;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql; 
+
+
+CREATE TRIGGER add_passageiros
+AFTER INSERT ON cliente_voo
+    FOR EACH ROW 
+    EXECUTE FUNCTION add_num_passageiros();
