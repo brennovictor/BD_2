ca73a4f90591fb39e75f5ede5c80b2aa5c0cc4d1
brennovictor <77638452+brennovictor@users.noreply.github.com>
2022-05-23 21:25:52 -0300

    adicionando delete cascade nas foreign keys

--- tarefas/t02/creates.sql
+++ tarefas/t02/creates.sql
@@ -5,7 +5,7 @@ CREATE TABLE cliente (
 	PRIMARY KEY (codigo) 
 );
 
-CREATE TABLE piloto ( 
+CREATE TABLE piloto (    
 	codigo SERIAL NOT NULL,
 	nome VARCHAR(50) NOT NULL, 
 	num_voos INTEGER NOT NULL,
@@ -13,27 +13,28 @@ CREATE TABLE piloto (
 );
 
 CREATE TABLE voo ( 
-	codigo SERIAL NOT NULL,
-        tipo VARCHAR(10) NOT NULL,
+	codigo SERIAL NOT NULL, 
+        tipo VARCHAR(10),
 	cod_piloto INTEGER NOT NULL,
-	distancia DECIMAL(10,2) NOT NULL,
+	num_passageiros INTEGER,
+	distancia DECIMAL(10,2),
 	PRIMARY KEY (codigo),
-	FOREIGN KEY (cod_piloto) REFERENCES piloto(codigo)
+	FOREIGN KEY (cod_piloto) REFERENCES piloto(codigo) ON DELETE CASCADE
 );
 
 CREATE TABLE milhas (
         cliente INTEGER NOT NULL,
         quantidade INTEGER NOT NULL,
         PRIMARY KEY(cliente),
-        FOREIGN KEY(cliente) REFERENCES cliente(codigo)
+        FOREIGN KEY(cliente) REFERENCES cliente(codigo) ON DELETE CASCADE
 
 );
 
-CREATE TABLE cliente_voo (
+CREATE TABLE cliente_voo ( 
         cliente INTEGER NOT NULL,
         voo INTEGER NOT NULL,
         classe VARCHAR(5) NOT NULL,
         PRIMARY KEY(cliente, voo),
-        FOREIGN KEY(cliente) REFERENCES cliente(codigo),
-        FOREIGN KEY(voo) REFERENCES voo(codigo)
+        FOREIGN KEY(cliente) REFERENCES cliente(codigo) ON DELETE CASCADE, 
+        FOREIGN KEY(voo) REFERENCES voo(codigo) ON DELETE CASCADE
 );
