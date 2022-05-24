32e957a2d4fa6ba3d6708dedf39c5f6464b87c23
brennovictor <77638452+brennovictor@users.noreply.github.com>
2022-05-23 21:26:56 -0300

    Update tarefa02-q08.sql

--- tarefas/t02/tarefa02-q08.sql
+++ tarefas/t02/tarefa02-q08.sql
@@ -1,1 +1,18 @@
+/* 08. Crie um gatilho que delete todos os clientes de um voo sempre que aquele voo for
+deletado e mais um gatilho para deletar o piloto. (Para efeito de informação, considere
+que o avião caiu). */
 
+CREATE FUNCTION del_cliente_voo()
+RETURNS trigger AS $$
+BEGIN
+    DELETE FROM cliente_voo
+    WHERE voo = NEW.codigo;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER del_cliente
+AFTER DELETE ON voo
+    FOR EACH ROW 
+    EXECUTE FUNCTION del_cliente_voo();
